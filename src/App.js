fa95bceb049a6443dd978ca72f8e864184db7686
Sneha Sawla <snehasawla2409@gmail.com>
2021-06-01 00:01:29 +0530

    navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CardDesign from './card/form';
+import Head from './navbar/navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Head />
       </header>
       <CardDesign />
     </div>
--- src/card/form.js
+++ src/card/form.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import './card.css'
+import './card.css';
 
 function CardDesign(){
     const [user, setsUser] = useState({});
@@ -23,16 +23,15 @@ function CardDesign(){
                             <div className="col-10 col-md-3 mt-5">
                                 <div className="card p-2" >
                                     <div className="d-flex">
-                                        <div className="img ml-4 d-flex mb-0 mt-0">
-                                            <img className="card-img-top" src={user.feature_img} alt="/" width="193px" height="130px" />
-                                        </div>
-                                        <div className="card-body ml-4" >
-                                            <small className="card-text">{user.category}</small> 
-                                            <h4 className="d-flex mb-0 mt-0 ">{user.title}</h4>
+                                        <div className="card-body">
+                                            <div classname="img"><img src={user.feature_img} alt="/" width="250px" height="150px" /></div>
+                                            
+                                            <small className="p-2 mt-2 d-flex justify-content-between card-category">{user.category}</small> 
+                                            <h5 className="p-2 mt-2 d-flex justify-content-between card-text">{user.title}</h5>
                                             <div className="p-2 mt-2 d-flex justify-content-between flex-column textLeft">
-                                                <div className="d-flex "><span className="shares">{user.pub_date}</span></div>
+                                                <div className="d-flex "><span className="author">{user.pub_date}</span></div>
                                                 <div className="d-flex "><span className="shares">{Date.now- user.pub_date}|</span> <span className="number1">{user.readtime}</span></div>
-                                            </div>  
+                                            </div> 
                                         </div>
                                     </div>
                                 </div>
